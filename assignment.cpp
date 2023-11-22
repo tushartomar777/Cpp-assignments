332c18d159f89678db4da322cfde96a146e2c66b
TUSHAR SINGH TOMAR <122335039+tushartomar777@users.noreply.github.com>
2023-11-22 20:38:57 +0530

    Add files via upload

---
+++ assignment.cpp
@@ -0,0 +1,49 @@
+// ----------------------------------------------------------------------------------------------------------------
+// // Ques: How can you output “Physics”and“Wallah” in two different lines in C++?
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"physics"<<endl<<"Wallah";
+// }
+// -----------------------------------------------------------------------------------------------------------------
+//       Ques: Print 10 using 2 positive numbers less than 6 in C++ as output.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<5+5<<endl;
+// }
+// -----------------------------------------------------------------------------------------------------------------
+//        Ques: How much space does following data types take ?
+// #include<iostream>
+// using namespace std;
+//  int main(){
+//      int a =1;
+//     bool flag = true;
+//     float c =22.2;
+
+//     cout<<sizeof(a)<<endl;
+//     cout<<sizeof(flag)<<endl;
+//     cout<<sizeof(c);
+//  }
+// -----------------------------------------------------------------------------------------------------------------
+//        Ques: What is the output of this program?
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a = 4;
+//     int b = 5;
+//     a ++;
+//     b --;
+//     cout << ++a <<" "<< b-- ;
+// }
+// -----------------------------------------------------------------------------------------------------------------
+//        ques: WAP to find circumferenceof a circle?
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 23;
+//     cout<<2*3.14*a;
+// }
+// -----------------------------------------------------------------------------------------------------------------
